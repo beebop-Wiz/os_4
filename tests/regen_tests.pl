d1342a64316bae35e1b5792aea9409413ac6c293
Blake Thomas <beebop@higgs.local>
2015-10-25 09:59:12 -0700

    Updated some testing stuff (better regen_tests)

--- tests/regen_tests.pl
+++ tests/regen_tests.pl
@@ -1,4 +1,19 @@
 use strict;
+open SRC_LIST, "../src/CMakeLists.txt";
+`cp CMakeLists.txt CMakeLists.orig`;
+open NEW_LIST, ">CMakeLists.txt";
+open OLD_LIST, "CMakeLists.orig";
+my $srcs;
+while(<SRC_LIST>) {
+    if(/add_executable\(os_4\.bin ((?:[a-zA-Z0-9_.]+ ?)+)\)/) {
+	$srcs = $1;
+    }
+}
+while(<OLD_LIST>) {
+    s/set\(KERNEL_SRCS ((?:[a-zA-Z0-9_.]+ ?)+)\)/set(KERNEL_SRCS $srcs)/g;
+    print NEW_LIST;
+}   
+`rm CMakeLists.orig`;
 open LISTFILE, ">cmake_tests.txt";
 open TESTFILE, ">cmake_add_test.txt";
 my @files = split /\n/, `ls src`;
